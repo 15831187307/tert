b38010e08f865434713b5e17274a49aa826c925e
李清振 <@DESKTOP-F54IOT2>
2018-01-15 17:40:02 +0800

    rrr

--- laiy/src/com/ldgx/test/ClassTest.java
+++
@@ -1,9 +0,0 @@
-package com.ldgx.test;
-
-public class ClassTest {
-	public static void main(String[] args) {
-		String name="userName";
-		System.out.println("我是一个兵");
-	}
-
-}
---
+++ lazz/src/com/ldgx/test/KTest.java
@@ -0,0 +1,7 @@
+package com.ldgx.test;
+
+public class KTest {
+  public static void main(String[] args) {
+	System.out.println("ddddddddd");
+}
+}
